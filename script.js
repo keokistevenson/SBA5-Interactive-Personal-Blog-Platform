50e33cdccd9d5443f801e75b18121c00c14678b0
keokistevenson <keokistevenson@gmail.com>
2026-03-29 22:22:02 -0400

    Cleaned up HTML more and created JS controls.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Checking if script is connected.");
+// alert("Checking if script is connected.");
+
+// FORM CONTROLS
+const modeHeader        = document.getElementById("blog-mode");
+
+const txtTitle          = document.getElementById("title");
+const msgTitle          = document.getElementById("title-error");
+
+const txtContent        = document.getElementById("content");
+const characterCount    = document.getElementById("character-count");
+const msgContent        = document.getElementById("content-error");
+
+const btnSave           = document.getElementById("Save");
+const btnCancel         = document.getElementById("Cancel");
+
+// POSTS
+const lblNoPosts         = document.getElementById("no-posts");
